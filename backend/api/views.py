1e70912736e036b298ad5db99a5e76e7b73c31c4
Lewis <lewisryework@gmail.com>
2026-02-28 18:16:44 +0000

    feat: add google api integration

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,14 @@
 # Create your views here.
+import os
+
+import requests
+from dotenv import load_dotenv
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 
 class LocationSerializer(serializers.Serializer):
@@ -19,4 +26,18 @@ class GetPoisFromLocation(APIView):
         lng = serializer.validated_data['lng']
         rad = serializer.validated_data['rad']
 
-        return Response({"pois": (lat, lng, rad)})
+        url = "https://places.googleapis.com/v1/places:searchText"
+
+        headers = {"Content-Type": "application/json", "X-Goog-Api-Key": GOOGLE_API_KEY,
+            "X-Goog-FieldMask": "*"}
+
+        data = {"textQuery": "restaurants in Brighton, England", "maxResultCount": 5}
+
+        response = requests.post(url, headers=headers, json=data)
+
+        if response.status_code == 200:
+            print(response.json())
+            return Response({"pois": (lat, lng, rad), "google_response": response.json()})
+        else:
+            print(f"Error {response.status_code}: {response.text}")
+            return Response({"pois": (lat, lng, rad)})
